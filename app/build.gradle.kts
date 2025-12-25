22f5d958576327efcc9f2e59425b31f8a7808e6c
NAGAR <mmm30001230\@gmail.com>
2025-12-25 13:02:37 +0200

    Feat: Add Firebase Realtime Database and Firestore
    This commit integrates Firebase Realtime Database and Firestore into the application.
    
    It introduces the necessary dependencies and configuration:
    - Adds the `google-services.json` file to the `app` module for Firebase project configuration.
    - Includes the `com.google.gms.google-services` plugin in both the project-level and app-level `build.gradle.kts` files.
    - Adds `firebase-database` and `firebase-firestore` dependencies to `libs.versions.toml` and `app/build.gradle.kts`.
    - Updates the Firebase Bill of Materials (`firebaseBom`) to version `34.7.0`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    id("com.google.gms.google-services")
 
 }
 
@@ -97,6 +98,8 @@ dependencies {
     // Room
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
+    implementation(libs.firebase.database)
+    implementation(libs.firebase.firestore)
     ksp(libs.androidx.room.compiler)
 
     testImplementation(libs.junit)
